68dd4e6d3d9e38bc39930030a5b6962b8df66053
Wojtek <vopox07@gmail.com>
2020-11-01 14:28:29 +0100

    Create project. One button, one text view

---
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -0,0 +1,25 @@
+package com.example.diceroller
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+import java.util.*
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+        buttonD6.setOnClickListener {
+            resultTextView.text = randomize(6)
+        }
+
+    }
+
+    private fun randomize(diceType: Int): String {
+        val random = Random()
+        val number = random.nextInt(6) + 1
+        return number.toString()
+    }
+}
